e36c36c327b045d8dfd4862a9ee030493fcc19d0
洪学臣 <stevehum14@gmail.com>
2021-02-05 13:44:25 +0800

    新建博文表单

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
+// router := mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
@@ -18,6 +22,25 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 func articsleShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -50,9 +73,9 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 func main() {
 	// router := http.NewServeMux()
-	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	router.HandleFunc("/articles/{id:[0-9]+}", articsleShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
@@ -60,11 +83,11 @@ func main() {
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	//中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
+	// router.Use(forceHTMLMiddleware)
 	//通过命名路由获取 url 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL:", articleURL)
+	// homeURL, _ := router.Get("home").URL()
+	// fmt.Println("homeURL", homeURL)
+	// articleURL, _ := router.Get("articles.show").URL("id", "23")
+	// fmt.Println("articleURL:", articleURL)
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
